d8ddcd12db9a5668cad7b98ee4bb800d97cc5cfd
gopuka <107343100+gopuka@users.noreply.github.com>
2023-06-04 05:39:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-kut2sm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-kut2sm)
---
+++ src/my-first-app/my-first-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-first-app',
+  templateUrl: './my-first-app.component.html',
+  styleUrls: ['./my-first-app.component.css']
+})
+export class MyFirstAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
